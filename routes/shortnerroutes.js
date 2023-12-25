a5a5bf65d6e8c41fe540f05469aed7f1275923a5
AVTAR SINGH <avtarsingh@AVTARs-MacBook-Air.local>
2023-12-26 00:12:43 +0530

    Added Testing EndPoint

--- routes/shortnerroutes.js
+++ routes/shortnerroutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const Router = express.Router();
 const {storeUrl,testApi,redirectUrl} = require('./methods');
 
-// Router.get('/',testApi);
+Router.get('/',testApi);
 Router.post('/shortner',storeUrl);
 Router.get('/:id',redirectUrl);
 
